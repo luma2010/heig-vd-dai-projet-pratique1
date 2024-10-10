4560b34d7a62e7a55634330f1730a2d2383b0487
Nekoth <alex@P51-Alex>
2024-10-10 19:46:12 +0200

    Add the function Delete

--- src/main/java/ch/heigvd/dai/commands/Delete.java
+++ src/main/java/ch/heigvd/dai/commands/Delete.java
@@ -1,4 +1,53 @@
 package ch.heigvd.dai.commands;
 
-public class Delete {
+
+import picocli.CommandLine;
+import ch.heigvd.dai.Main;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@CommandLine.Command(name = "Delete", description = "Delete a variable to a file")
+public class Delete implements Runnable {
+    @CommandLine.ParentCommand protected Main parent;
+
+    @CommandLine.Parameters(
+            paramLabel = "<varName>",
+            description = "Name of the variable to delete"
+    )
+    private String varName;
+
+    @Override
+    public void run(){
+        try{
+            InputStream in = new FileInputStream(parent.getFileName());
+            Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(reader);
+
+            // Sert à la lecture du fichier.
+            List<String> lines = new ArrayList<String>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(!Objects.equals(br.readLine(), varName)){
+                    lines.add(line+"\n");
+                }
+            }
+            br.close();
+
+            OutputStream os = new FileOutputStream(parent.getFileName());
+            Writer writer = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+            BufferedWriter bw =new BufferedWriter(writer);
+
+            for (String s : lines) {
+                bw.write(s);
+            }
+            bw.flush();
+
+        } catch(IOException e){
+            System.out.println("Exeption" +e);
+        }
+    }
 }
